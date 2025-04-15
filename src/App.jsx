c322f3870c5712446d5a017c810532787ae77b8d
Timothy Waweru <timothy.waweru1@student.moringaschool.com>
2025-04-15 12:55:32 +0300

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <div className='details'>
       <h1>Expense Tracker</h1>
       <p>Start taking control of your finances and life.Record,catalyze  and analyze your spending </p>
     </div>
--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const ExpenseForm = () => {
   return (
-    <div>
+    <div className='form'>
         <h3>Add Expense</h3>
         <p>Enter your details below</p>
         <form>
-            <input type='text' placeholder='Enter expense name'/>
-            <input type='text' placeholder='Enter expense description'/>
-            <input type='text' placeholder='Enter expense category'/>
-            <input type='text' placeholder='Enter amount'/>
-            <input type='date' placeholder=''/>
+            <input type='text' placeholder='Enter expense name' required/>
+            <input type='text' placeholder='Enter expense description'required/>
+            <input type='text' placeholder='Enter expense category'required/>
+            <input type='text' placeholder='Enter amount' required/>
+            <input type='date' placeholder='' required/>
             <button>Submit</button>
         </form>
     </div>
